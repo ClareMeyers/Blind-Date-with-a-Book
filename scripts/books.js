a0079421823a14e2952daf715a1ae100f75fb032
Clare Meyers <claremeyers91@gmail.com>
2023-12-10 10:25:18 -0500

    Info Update

--- scripts/books.js
+++ scripts/books.js
@@ -1,15 +1,25 @@
-// books.js
+// inputs in each book's button info by book number 
 var books = {
     1: {
-        author: "This book was written by a Queer American author who grew up in Louisiana and currently lives in New York City.",
-        quotes: "“The phrase 'see attached bibliography' is the single sexiest thing you have ever written to me.”",
-        tropes: "Enemies to lovers, Queer Romance, Alternate history of the 21st century, The burden of fame"
+        tropes: 'Enemies to lovers, Queer Romance, Alternate history of the 21st century, The burden of fame',
+        author: 'This book was written by a Queer American author who grew up in Louisiana and currently lives in New York City.',
+        quote: '“The phrase "see attached bibliography" is the single sexiest thing you have ever written to me.”',
     },
     2: {
-        name: 'another book',
-        author: 'another author',
-        tropes: 'another synopsis'
-    }
+        tropes: 'Enemies to Lovers, Fake dating, Stuck together',
+        author: 'This book was written by two longtime writing partners and best friends who operate under one combined pen name.',
+        quote: '“I’m a terrible liar.” <p>“Really? You hid it so well.” </p> “It’s never been my strength, okay? Those of us who aren’t summoned by the Dark Mark consider honesty to be a virtue.”',
+    },
+    3: {
+        tropes: 'Queer Romance, The burden of fame, Forbidden love, Found family, Rags to riches, Anti-hero archetype',
+        author: 'This book was written by an American author with four New York Times Bestselling novels(including this one), one of which was adapted into an Amazon original miniseries(not this one).',
+        quote: '“You do not know how fast you have been running, how hard you have been working, how truly exhausted you are, until somewhat stands behind you and says, “It’s OK, you can fall down now. I’ll catch you.”',
+    },
+    4: {
+        tropes: 'Love triangle with brothers, Coming-of-age, Sunny vs grumpy',
+        author: 'This #1 New York Times bestselling author has received notable acclaim over her two YA romance trilogies, one of which was adapted into an Amazon original series(it might be this one).',
+        quote: '"When you walk on the beach at night, you can say things you can’t say in real life. In the dark you can feel really close to a person. You can say whatever you want.”',
+    },
 };
 
 var isTropesvisible = {};
@@ -28,6 +38,7 @@ function revealTropes (bookNumber) {
     isTropesvisible[bookNumber] = !isTropesvisible[bookNumber]; // toggles
 }
 
+
 var isAuthorvisible = {};
 
 function revealAuthor(bookNumber){
@@ -42,4 +53,21 @@ function revealAuthor(bookNumber){
         authorElement.innerHTML = books[bookNumber].author; //reveals
     }
     isAuthorvisible[bookNumber] = !isAuthorvisible[bookNumber]; // toggles
+}
+
+
+var isQuotevisible = {};
+
+function revealQuote (bookNumber) {
+    console.log(books[bookNumber].quote);
+    var quoteElement = document.getElementById("book" + bookNumber + "-quote");
+
+    // Hides and reveals button when clicked
+    if (isQuotevisible[bookNumber]) {
+        quoteElement.innerHTML = ""; // clears
+    } else {
+        console.log(books[bookNumber].quote);
+        quoteElement.innerHTML = books[bookNumber].quote; //reveals
+    }
+    isQuotevisible[bookNumber] = !isQuotevisible[bookNumber]; // toggles
 }
